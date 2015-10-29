d149ec3945057d7bd3c1e5bc1bf0ad887929aca0
Trex is roaring after successful compilation of his code <shakirovartur28@gmail.com>
2015-10-29 14:21:23 -0400

    Update main.cpp

--- CustomVector/CustomVector/main.cpp
+++ CustomVector/CustomVector/main.cpp
@@ -5,19 +5,23 @@
 int main() {
 	artur::arturVector<double> prices; // initially empty
 
-	if (prices.empty())       // is prices empty?
+	if (prices.empty())       // checks whether price is empty
 		std::cout << "prices is empty" << std::endl;
 	prices.push_back(23.56);
 	prices.push_back(13.67);
-	
-	prices.back() = 20.25;
-	prices.push_back(300.56);
-	prices.push_back(45.67);
-	ptices.pop_back();
-	
+	prices.push_back(100.55);
+
+	prices.back() = 87.99;
 	for (int i = 0; i < prices.size(); i++)
 		std::cout << prices[i] << " ";
 	std::cout << std::endl;
 
+	prices.pop_back();
+	for (int i = 0; i < prices.size(); i++)
+		std::cout << prices[i] << " ";
+	std::cout << std::endl;
+
+	std::cout << prices.at(0) << std::endl;
+	system("pause");
 	return 0;
 }
